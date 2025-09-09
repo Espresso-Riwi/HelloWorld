c4b64e8b4d69e471ada22465638014e11382eb96
JffersonGp <Jffers0n.Gp@gmail.com>
2025-09-08 21:30:43 -0500

    feat: add Helloworld java file

---
+++ Helloworld.java
@@ -0,0 +1,13 @@
+public class Helloworld{
+
+    // Variable con nombre
+
+    public static void main(String[] args){
+
+        String name = "Jefferson";
+
+        System.out.println("Hello World " + name);
+
+
+    }
+}
