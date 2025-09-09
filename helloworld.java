ef5d2ef537f97c405f1360ed60ad7ddba42cb8d3
Samuel Rosero Alvarez <p3g455o48@gmail.com>
2025-09-08 21:23:31 -0500

    feat: Added a helloworld.java file

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld {
+
+    public static void main(String[] args) {
+        String myName = "Samuel Rosero Alvarez";
+
+        System.out.println("This is my first project in Java, Hello world! And my name is: " + myName);
+    }
+}
